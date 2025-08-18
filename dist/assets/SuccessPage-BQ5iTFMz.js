37ff629407b99080d85a9bb738ba505bde8e9cac
avi074 <gautamavinash.ag@gmail.com>
2025-08-18 13:07:36 +0000

    build v3

---
+++ dist/assets/SuccessPage-BQ5iTFMz.js
@@ -0,0 +1,1 @@
+import{z as e,j as s,B as a}from"./index-DEEnX9rt.js";import{R as r}from"./index-Bd18UBJ4.js";function c(){const t=e();return s.jsx(r,{status:"success",title:"Payment Successful!",subTitle:"Your subscription or credit purchase has been processed.",extra:[s.jsx(a,{type:"primary",onClick:()=>t("/"),children:"Go to Dashboard"})]})}export{c as default};
