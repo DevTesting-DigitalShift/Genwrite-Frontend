266df43f4c48ba5f938f7107011d97920b0584a5
avi074 <gautamavinash.ag@gmail.com>
2025-08-18 13:07:36 +0000

    build v3

--- dist/assets/CancelPage-D8zfRJFZ.js
+++
@@ -1,1 +0,0 @@
-import{J as a,j as e,E as s}from"./index-Cd0i5XqR.js";import{R as n}from"./index-DAiDXwRF.js";function l(){const t=a();return e.jsx(n,{status:"error",title:"Payment Cancelled",subTitle:"You cancelled the payment. No changes were made.",extra:[e.jsx(s,{onClick:()=>t("/"),children:"Return to Dashboard"})]})}export{l as default};
