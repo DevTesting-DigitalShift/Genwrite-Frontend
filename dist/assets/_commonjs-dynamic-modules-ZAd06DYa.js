d471ef052aacb01a4fdc0e519246f358123fb408
avi074 <gautamavinash.ag@gmail.com>
2025-08-25 03:06:00 +0000

    dist removed

--- dist/assets/_commonjs-dynamic-modules-ZAd06DYa.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./createLucideIcon-DG6wBJdZ.js";/**
- * @license lucide-react v0.488.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const r=[["path",{d:"M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71",key:"1cjeqo"}],["path",{d:"M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",key:"19qd67"}]],i=e("link",r);function n(o){throw new Error('Could not dynamically require "'+o+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}export{i as L,n as c};
